3ef96a6fc006ca2541244032c4604ced3bb221a8
unknown <hosseingoody13@gmail.com>
2022-03-30 16:35:50 +0430

    Add Utiliti folder that contains cookie setting

---
+++ Src/EndPoint.Site/Utilities/CookiesManager.cs
@@ -0,0 +1,49 @@
+﻿namespace EndPoint.Site.Utilities
+{
+    public class CookiesManager
+    {
+        //adding Cookie
+        public void Add(HttpContext context, string token, string value)
+        {
+            context.Response.Cookies.Append(token, value, getCookieOptions(context));
+        }
+
+        //Checks if there is a cookie
+        public bool Contains(HttpContext context, string token)
+        {
+            return context.Request.Cookies.ContainsKey(token);
+        }
+
+        //Get Value
+        public string GetValue(HttpContext context, string token)
+        {
+            string cookieValue;
+            if (!context.Request.Cookies.TryGetValue(token, out cookieValue))
+            {
+                return null;
+            }
+            return cookieValue;
+        }
+
+        //Delete of Cookie
+        public void Remove(HttpContext context, string token)
+        {
+            if (context.Request.Cookies.ContainsKey(token))
+            {
+                context.Response.Cookies.Delete(token);
+            }
+        }
+
+        //Set options for cookies
+        private CookieOptions getCookieOptions(HttpContext context)
+        {
+            return new CookieOptions
+            {
+                HttpOnly = true,
+                Path = context.Request.PathBase.HasValue ? context.Request.PathBase.ToString() : "/",
+                Secure = context.Request.IsHttps,
+                Expires = DateTime.Now.AddDays(100),
+            };
+        }
+    }
+}
