650f5d7b6aeacef7b2f7852c9c6191096cb20f05
unknown <hosseingoody13@gmail.com>
2022-04-04 22:16:17 +0430

    Add ClaimUtility in UtilityFolder for connect the cart to user

---
+++ Src/EndPoint.Site/Utilities/ClaimUtitlity.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+
+namespace EndPoint.Site.Utilities
+{
+    public static class ClaimUtitlity
+    {
+        public static int? GetUserId(ClaimsPrincipal User)
+        {
+            try
+            {
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+
+                if (claimsIdentity.FindFirst(ClaimTypes.NameIdentifier) != null)
+                {
+                    int userId = int.Parse(claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value);
+                    return userId;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+
+        }
+    }
+}
