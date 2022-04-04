89b59c0d556639221a92dc6299e8bb5851e34803
unknown <hosseingoody13@gmail.com>
2022-04-04 22:18:13 +0430

    Add Cart viewcomponent for all pages

---
+++ Src/EndPoint.Site/ViewComponents/Carts/Cart.cs
@@ -0,0 +1,22 @@
+﻿using EndPoint.Site.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using TemplateProjectOne.Application.Services.Carts;
+
+namespace EndPoint.Site.ViewComponents.Cart
+{
+    public class Cart:ViewComponent
+    {
+        private readonly ICartServices _cartServices;
+        private readonly CookiesManager cookies;
+        public Cart(ICartServices cartServices)
+        {
+            _cartServices = cartServices;
+            cookies = new CookiesManager();
+        }
+        public IViewComponentResult Invoke()
+        {
+            var userId = ClaimUtitlity.GetUserId(HttpContext.User);
+            return View(viewName: "Cart", _cartServices.GetCart(cookies.GetBrowserId(HttpContext) , userId).Data);
+        }
+    }
+}
