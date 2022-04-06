e8098e17cb460f901fabf1490672315cd17af723
unknown <hosseingoody13@gmail.com>
2022-04-06 14:25:27 +0430

    Add RequestPay Entity

---
+++ Src/TemplateProjectOne.Domain/Entities/Fainance/RequestPay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateProjectOne.Domain.Entities.Common;
+using TemplateProjectOne.Domain.Entities.Users;
+
+namespace TemplateProjectOne.Domain.Entities.Fainance
+{
+    public class RequestPay:BaseEntity
+    {
+        public Guid Guid { get; set; }
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; }
+        public bool isPay { get; set; }
+        public DateTime? DatePay { get; set; }
+        public string Authority { get; set; } = " ";
+        public long RefId { get; set; } = 0;
+    }
+}
