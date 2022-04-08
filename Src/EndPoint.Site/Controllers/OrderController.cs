aaaed3bcd986626c1f3f0eac2fb41b9e08eff139
unknown <hosseingoody13@gmail.com>
2022-04-08 23:40:07 +0430

    Add orderController

---
+++ Src/EndPoint.Site/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using EndPoint.Site.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TemplateProjectOne.Application.Services.Orders.Query.GetUserOrders;
+
+namespace EndPoint.Site.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IGetUserOrderServices _getUserOrder;
+        public OrderController(IGetUserOrderServices getUserOrder)
+        {
+            _getUserOrder = getUserOrder;
+        }
+        public IActionResult Index()
+        {
+            int userId = ClaimUtitlity.GetUserId(User).Value;
+            return View(_getUserOrder.Execute(userId).Data);
+        }
+    }
+}
