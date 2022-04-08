2c6804872a96e0abd3e38c3c3f0a8949636fb34a
unknown <hosseingoody13@gmail.com>
2022-04-08 23:43:21 +0430

    Add SignUpViewModel

--- Src/EndPoint.Site/Models/ViewModel/Authentication/SignUpViewModel.cs
+++ Src/EndPoint.Site/Models/ViewModel/Authentication/SignUpViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class SignUpViewModel
     {
-        public string FullName { get; set; } = " ";
+        public string Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? RePassword { get; set; }
