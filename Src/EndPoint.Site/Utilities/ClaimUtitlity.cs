43ff2613effea75ce8c0837555e59b7ccca3ca20
unknown <hosseingoody13@gmail.com>
2022-04-09 20:16:31 +0430

    Edit claimUtility

--- Src/EndPoint.Site/Utilities/ClaimUtitlity.cs
+++ Src/EndPoint.Site/Utilities/ClaimUtitlity.cs
@@ -28,5 +28,24 @@ namespace EndPoint.Site.Utilities
             }
 
         }
+
+        public static List<string> GetRoles(ClaimsPrincipal User)
+        {
+            try
+            {
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+                List<string> roles = new List<string>();
+                foreach (var item in claimsIdentity.Claims.Where(p => p.Type.EndsWith("role")))
+                {
+                    roles.Add(item.Value);
+                }
+                return roles;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
     }
 }
